5d459442ea159d88e32fb0983f3d13687d77c202
MAHADIHASSAN444 <mail.mahadihassan@gmail.com>
2020-08-07 12:20:08 -0700

    firsr one i ready to push

---
+++ true-or-false.js
@@ -0,0 +1,17 @@
+// falsey
+// false
+// 0
+// ""
+// undefined
+// null
+// nun
+
+// truthy
+// "0" ,' ', [];
+ let name = 0;
+if(name || name == 0 ){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
