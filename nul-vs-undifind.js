9c760f4a3fbd454bcdf5883efdf031c5551d2feb
MAHADIHASSAN444 <mail.mahadihassan@gmail.com>
2020-08-07 13:14:36 -0700

    undifined is now clear in my mind

---
+++ nul-vs-undifind.js
@@ -0,0 +1,16 @@
+let pakhi;
+
+function add(name1 , name2) {
+    console.log(name1 +name2);
+    return
+}
+
+function add(name1 , name2) {
+    console.log(name1 ,name2);
+
+const preamik = {name:"preamik purush",phone: 9490,}
+console.log(preamik.gf);
+let fun = undefined;
+console.log(fun);
+let age =[21, 12, 23];
+console.log(age[7]);
