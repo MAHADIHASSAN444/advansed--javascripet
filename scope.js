97a301ceeecbb047be86b830ccf92f869caadf18
MAHADIHASSAN444 <mail.mahadihassan@gmail.com>
2020-08-08 14:12:00 -0700

    i havr learn many thing today

---
+++ scope.js
@@ -0,0 +1,13 @@
+const bonnas = 20;
+function sum(first, second){
+let result = first + second+ bonnas;
+// console.log(bonnas);
+if (result > 9) {
+    const mood = "happy";
+    console.log(mood);
+}
+return result;
+}
+const output = sum(5,8);
+// console.log(bonnas);
+// console.log(output);
